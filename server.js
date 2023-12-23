0ad3b1c781bc480e53f79d75c65b8a7319595890
Med5KDB <kevindebruyne17med@gmail.com>
2023-12-23 12:45:45 +0000

    use nodemon to enable starting my server with auto-restart

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { exec } = require('child_process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,8 +9,8 @@ app.post('/webhook', options, (req, res) => {
     res.status(202).send("Accepted");
 
     const githubEvent = req.headers['x-github-event'];
-    console.log(githubEvent);
-    console.log(PORT);
+    console.log("Req header: " + JSON.stringify(req.headers));
+    console.log("The github event: "+ githubEvent);
     if(githubEvent === "pull_request") {
         const data = req.body;
         const action = data.action;
@@ -20,6 +21,37 @@ app.post('/webhook', options, (req, res) => {
 
 })
 
+function launchBuildProcess() {
+    exec("npm install", (err, stdout, stderr) => {
+        if(err) {
+            console.error(`An error occured when executing npm install: ${err}`);
+        }
+        if(stderr) {
+            console.error("npm install: " + stderr);
+        }
+        console.log("npm install..." + stdout);
+        exec("npm build")
+    })
+}
+
 app.listen(PORT, () => {
-    console.log(`CI server is up and listening on ${PORT}`)
+    console.log(`CI server is up and listening for events at ${PORT}`)
 })
+
+const SmeeClient = require('smee-client');
+const { stdout, stderr } = require('process');
+
+const smee = new SmeeClient({
+  source: 'https://smee.io/NCLblu2qxIE7C0i',
+  target: `http://localhost:${PORT}/events`,
+  logger: console
+})
+
+const events = smee.start()
+
+// Stop forwarding events
+process.on('SIGINT', () => {
+    console.log("Stopping event forwarding and shutting down server...");
+    events.close();
+    server.close();
+});
